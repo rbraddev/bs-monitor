2792c5aed0cd4b86b8b3eb86b3b966d391f3bd5f
Ryan Bradshaw <ryan@rbradshaw.dev>
2020-11-04 22:31:41 +0000

    working on async monitor

--- monitor/monitor.py
+++ monitor/monitor.py
@@ -2,6 +2,7 @@ import sys
 import asyncio
 from typing import List
 
+import aiofile
 import yaml
 from yaml.scanner import ScannerError
 
@@ -12,8 +13,11 @@ available_sites = {"snipes": tasks.Snipes}
 
 
 class Monitor:
-    def __init__(self, proxy_file: str = "proxies.txt", monitors_file: str = "monitors.yml"):
+    def __init__(self, proxy_file: str = "proxies.txt", monitors_file: str = "monitors.yml", max_workers: int = 10):
         self.proxies: List[Proxy] = []
+        self.q: asyncio.Queue = None
+        self.monitor_list: list = []
+        self.max_workers: int = max_workers
 
         self._initiate_proxies(proxy_file)
         self._validate_monitor_file(monitors_file)
@@ -51,5 +55,34 @@ class Monitor:
             for item in data["items"]:
                 print(f"  - {item}")
 
+    async def _update_monitor(self):
+        while True:
+            try:
+                async with aiofiles.open("test_tasks.yml") as f:
+                    try:
+                        monitors = await f.read()
+                        monitors = yaml.safe_load(tasks)
+                    except ScannerError:
+                        print("There is an error in the monitor file, continuting with the currently loaded monitor list.")
+            except FileNotFoundError:
+                print("Monitor file is missing, continuing with current loaded monitor list")
+
+            for monitor in monitors:
+                if monitor not in self.monitor_list:
+                    self.monitor_list.append(task)
+                    await self.q.put(task)
+            await asyncio.sleep(5)
+
+    async def _start_tasks(self):
+        self.q = asyncio.Queue()
+        tasks = [asyncio.create_task(self.update_tasks())]
+        for _ in range(self.max_workers):
+            asyncio.create_task(self._worker())
+        await asyncio.gather(*tasks)
+        await self.q.join()
+
     def run(self):
-        pass
+        try:
+            asyncio.run(self._start_tasks())
+        except KeyboardInterrupt:
+            print("Monitor exited")
