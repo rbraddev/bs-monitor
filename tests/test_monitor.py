dc1449d07d98921ba4f7d1b493b5ea776433883b
Ryan Bradshaw <ryan@rbradshaw.dev>
2020-11-11 12:51:04 +0000

    removed tests

--- tests/test_monitor.py
+++
@@ -1,8 +0,0 @@
-from monitor.monitor import Monitor
-from monitor.proxy import Proxy
-
-
-def test_monitor_obj_succesful():
-    monitor = Monitor(proxy_file="test_proxy.txt")
-    assert len(monitor.proxies) == 1
-    assert isinstance(monitor.proxies[0], Proxy)
